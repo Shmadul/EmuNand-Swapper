093c8482c3459917dec955237cbeb97d3994c28a
Mason <masonpc@live.com>
2019-07-12 17:25:42 -0400

    Add files via upload

--- source/main.c
+++ source/main.c
@@ -6,27 +6,54 @@
 // Include the main libnx system header, for Switch development
 #include <switch.h>
 
+#define MAXCHAR 1000
+//define integers
+int disable, middle, enable;
+
+//set paths
+char enabled[] = "/emuMMC/emummc.ini";
+char disabled[] = "/emuMMC/emummc.disabled";
+char inbetween[] = "/emuMMC/disabled.ini";
+
+//Read emummc config
+void readini()
+    {
+         FILE *fp = fopen(enabled, "r");
+         if(fp == NULL) 
+           {
+                perror("Unable to open emummc.ini");
+                exit(1);
+           }
+         char line[128];
+         while(fgets(line, sizeof(line), fp) != NULL) 
+              {
+                    if (strstr(line, "sector=") != NULL) 
+                       {
+                            fputs(line, stdout);
+                       } 
+                   else if (strstr(line, "title=") != NULL) 
+                       {
+                            fputs(line, stdout);
+                       } 
+                        
+              }
+
+         fclose(fp);
+    }
+
 // Main program entrypoint
 int main(int argc, char* argv[])
 {
     consoleInit(NULL);
 
-    //define integers
-    int disable, middle, enable;
 
-    //set paths
-    char enabled[] = "/emuMMC/emummc.ini";
-    char disabled[] = "/emuMMC/emummc.disabled";
-    char inbetween[] = "/emuMMC/disabled.ini";
-    
-    //Printf Stuff
-    printf("\033[1;32m");
-    printf("RAW Nand Swapper v1.0\n");
+    //Menu Stuff
+    printf("RAW Nand Swapper v2.1\n");
     printf("@shmadul 2019\n");
-    printf("\033[0m");
+    printf("Current Active Nand: \n");
+    readini();
     printf("\033[1;31m");
     printf("Press A to Swap Active Raw Nand\n");
-    printf("Press X to reboot\n");
     printf("Press + to exit\n");
     printf("\033[0m");
 
@@ -48,6 +75,11 @@ int main(int argc, char* argv[])
 
         if (kDown & KEY_A)
             {
+                //Clear screen
+                printf("\e[1;1H\e[2J");
+                printf("RAW Nand Swapper v2.1\n");
+                printf("@shmadul 2019\n");
+
                 //Allow for error Reporting
                 disable = rename(enabled, inbetween);
                 middle = rename(disabled, enabled);
@@ -74,15 +106,18 @@ int main(int argc, char* argv[])
                         printf("Error Renaming %s to %s \n", inbetween, disabled);
                         printf("\033[0m");
                   } else {
-                        printf("\033[1;36m");
+                        printf("\033[1;32m");
                         printf("Successfully Swapped Active Raw Nand\n");
                         printf("\033[0m");   
-
+                        printf("Current Active Nand: \n");
+                        readini();
                         printf("\033[1;31m");
                         printf("Press X to reboot, Press + to exit\n");
                         printf("\033[0m");
                     
                   }
+
+
             }
 
         if (kDown & KEY_X)
